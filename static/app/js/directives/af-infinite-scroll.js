308135a787c95cc9ae413a63ce5c5bf12de912d6
Wilson Jimenez <wilsonhjimenez@gmail.com>
2015-07-09 14:33:16 +0100

    Changed scroll function to debounced.

--- static/app/js/directives/af-infinite-scroll.js
+++ static/app/js/directives/af-infinite-scroll.js
@@ -1,10 +1,10 @@
-define(['directives/directives'], function(directives) {
+define(['directives/directives', 'lodash'], function(directives, _) {
 
   directives.directive('afInfiniteScroll', scroll);
 
-  scroll.$inject = ['$window', '$document', '$interval'];
+  scroll.$inject = ['$window', '$document', '$interval', '$timeout'];
 
-  function scroll($window, $document, $interval) {
+  function scroll($window, $document, $interval, $timeout) {
     var directive = {
       restrict: 'EA',
       link: link,
@@ -23,21 +23,30 @@ define(['directives/directives'], function(directives) {
       // removes events first to prevent multiple assignment
       angular.element($document)
         .off('scroll.infinite mousemove.idle keypress.idle click.idle')
-        .on('scroll.infinite', onDocumentScrollDown)
+        .on('scroll.infinite', _.debounce(onDocumentScrollDown, 300))
         .on('mousemove.idle keypress.idle click.idle', onDocumentKeyOrMouse);
 
       scope.idleTime = 0;
+      scope.$on('$destroy', cleanUp);
       var idleInterval = $interval(checkIdleTime, 5e3);
 
       // if idle for 10 secs, fetch the next batch without rendering it
       function checkIdleTime() {
         scope.idleTime += 5;
 
-        if (scope.idleTime > 5 && !scope.loading) {
+        // if there is no scroll, fetch the next batch on next digest cycle
+        if ($window.innerHeight >= $document[0].documentElement.scrollHeight) {
+          $timeout(onDocumentScrollDown);
+        } else if (scope.idleTime > 5 && !scope.loading) {
           scope.fetchIdleBatch();
           $interval.cancel(idleInterval); // cancel the interval to not fetch another batch until buffer is cleared
           scope.idleTime = 0;
         }
+      }
+
+      function cleanUp() {
+        angular.element($document).off('scroll.infinite mousemove.idle keypress.idle click.idle');
+        $interval.cancel(idleInterval);
       }
 
       function onDocumentKeyOrMouse() {
@@ -52,18 +61,11 @@ define(['directives/directives'], function(directives) {
 
         // resumes the interval
         if (idleInterval.$$state.value === 'canceled') {
-          idleInterval = $interval(checkIdleTime, 5000);
-        }
-
-        // removes event listener on end of catalogue and cancel the interval
-        if (scope.totalReached) {
-          angular.element($document).off('scroll.infinite mousemove.idle keypress.idle click.idle');
-          $interval.cancel(idleInterval);
-          return;
+          idleInterval = $interval(checkIdleTime, 5e3);
         }
 
         // fetches and render the next batch
-        if ($window.scrollY >= $document[0].documentElement.offsetHeight - $window.innerHeight) {
+        if ($window.scrollY >= ($document[0].documentElement.offsetHeight - $window.innerHeight - 350)) {
           scope.$apply(scope.onScrollDown);
         }
       }
